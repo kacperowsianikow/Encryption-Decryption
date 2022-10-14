4eff3ef45eacfdf6157d99a365076e1b4da91593
kacperowsianikow <kacper.owsianikow@gmail.com>
2022-10-14 16:38:48 +0200

    Added decryption

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,29 +3,73 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static int minAscii = 32;
+    private static int maxAscii = 126;
+    private static char[] inputCharacters;
+    private static int key;
+    private static String userInput;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select action:");
+        System.out.println("enc - encryption");
+        System.out.println("dec - decryption");
+        String selection = scanner.nextLine();
 
-        int minAscii = 97;
-        int maxAscii = 122;
+        switch (selection) {
+            case "enc" -> encryption();
+            case "dec" -> decryption();
+            default -> System.out.println("Incorrect input!");
+        }
+    }
 
-        System.out.println("Enter your message: ");
-        String userInput = scanner.nextLine();
-        userInput = userInput.toLowerCase();
+    private static void encryption() {
+        System.out.println("Enter your message to encrypt: ");
+        userInput = scanner.nextLine();
+        System.out.println("Enter your encryption key: ");
+        try {
+            key = scanner.nextInt();
+            scanner.nextLine();
+        }
+        catch (Exception e) {
+            System.out.println("Wrong value!");
+            return;
+        }
 
-        char[] inputCharacters = userInput.toCharArray();
+        inputCharacters = userInput.toCharArray();
         char[] encryptedCharacters = new char[userInput.length()];
 
         for (int i = 0; i < userInput.length(); i++) {
             int ascii = inputCharacters[i];
-            int encryptedAscii = ascii;
-
-            if ((ascii >= minAscii) && (ascii <= maxAscii)) {
-                encryptedAscii = maxAscii - ascii + minAscii;
-            }
+            int encryptedAscii;
+            encryptedAscii = (ascii + key - minAscii) % 95 + minAscii;
             encryptedCharacters[i] = (char) encryptedAscii;
         }
         System.out.println(String.valueOf(encryptedCharacters));
+    }
 
+    private static void decryption() {
+        System.out.println("Enter your message to decrypt: ");
+        userInput = scanner.nextLine();
+        System.out.println("Enter your decryption key: ");
+        try {
+            key = scanner.nextInt();
+            scanner.nextLine();
+        }
+        catch (Exception e) {
+            System.out.println("Wrong value!");
+            return;
+        }
+
+        inputCharacters = userInput.toCharArray();
+        char[] decryptedCharacters = new char[userInput.length()];
+
+        for (int i = 0; i < userInput.length(); i++) {
+            int ascii = inputCharacters[i];
+            int decryptedAscii;
+            decryptedAscii = (ascii - key - minAscii) % 95 + minAscii;
+            decryptedCharacters[i] = (char) decryptedAscii;
+        }
+        System.out.println(String.valueOf(decryptedCharacters));
     }
 }
